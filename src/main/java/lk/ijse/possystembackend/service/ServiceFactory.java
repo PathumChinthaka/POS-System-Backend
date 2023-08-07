8d09b8ddc25afff3d759b2b4185128245e9ef899
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-08-07 14:24:59 +0530

    service factory created

---
+++ src/main/java/lk/ijse/possystembackend/service/ServiceFactory.java
@@ -0,0 +1,22 @@
+package lk.ijse.possystembackend.service;
+
+import lk.ijse.possystembackend.service.custom.impl.CustomerServiceImpl;
+import lk.ijse.possystembackend.service.custom.utill.ServiceTypes;
+
+public class ServiceFactory {
+    private static ServiceFactory serviceFactory;
+
+    public synchronized static ServiceFactory getInstance(){
+        return serviceFactory==null?serviceFactory=new ServiceFactory():serviceFactory;
+    }
+
+    public <T extends SuperService> T getService(ServiceTypes types){
+        switch (types){
+            case CUSTOMER_SERVICE:
+                return (T) new CustomerServiceImpl();
+            default:
+                throw new RuntimeException("Error: Invalid service Type");
+        }
+    }
+
+}
