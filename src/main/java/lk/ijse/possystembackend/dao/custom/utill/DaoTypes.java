1af085fd9ccd983b5ebf6cdd20ea19e62dd72e45
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-08-07 13:57:57 +0530

    enum types

---
+++ src/main/java/lk/ijse/possystembackend/dao/custom/utill/DaoTypes.java
@@ -0,0 +1,7 @@
+package lk.ijse.possystembackend.dao.custom.utill;
+
+public enum DaoTypes {
+    CUSTOMER_DAO,
+    ITEM_DAO,
+    ORDER_DAO
+}
---
+++ src/main/java/lk/ijse/possystembackend/service/custom/utill/ServiceTypes.java
@@ -0,0 +1,7 @@
+package lk.ijse.possystembackend.service.custom.utill;
+
+public enum ServiceTypes {
+    CUSTOMER_SERVICE,
+    ITEM_SERVICE,
+    ORDER_SERVICE
+}
